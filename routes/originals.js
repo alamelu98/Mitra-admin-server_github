1cf9f3929467bce08cdee78c88d0d56e5adcda0f
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2022-12-22 15:46:15 +0530

    Update originals.js

--- routes/originals.js
+++ routes/originals.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const router=express.Router()
-const {getAllOriginals,postOriginals,UpdateOriginals,deleteOriginals,getEachOriginals}=require("../controller/Originals")
+const {getAllOriginals,postOriginals,UpdateOriginals,deleteOriginals,getEachOriginals}=require("../controller/originals")
 
 
 
