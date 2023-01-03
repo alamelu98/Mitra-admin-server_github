1cd66413f093326b86837eb8ddf5ff8c2af18237
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2023-01-03 15:08:42 +0530

    Update prints.js

--- routes/prints.js
+++ routes/prints.js
@@ -2,16 +2,8 @@ const express=require("express")
 const router=express.Router()
 const {getAllPrints,postPrints,UploadImage,UpdatePrints,deletePrints,getEachPrints}=require("../controller/prints")
 const multer=require("multer")
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = file.originalname
-      console.log(file.originalname)
-      cb(null,uniqueSuffix)
-    }
-  })
+
+const storage = multer.diskStorage({})
 
   const fileFilter=(req,file,cb)=>
   {
