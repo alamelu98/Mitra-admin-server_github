b55e818fa5f17540dcd01294d6ba0f32101272de
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2023-01-05 13:50:51 +0530

    Update originals.js

--- routes/originals.js
+++ routes/originals.js
@@ -2,12 +2,26 @@ const express=require("express")
 const router=express.Router()
 const {getAllOriginals,postOriginals,UpdateOriginals,deleteOriginals,getEachOriginals}=require("../controller/originals")
 
+const multer=require("multer")
+
+const storage = multer.diskStorage({})
+
+  const fileFilter=(req,file,cb)=>
+  {
+    if(file.mimetype==='image/png' || file.mimetype === 'image/jpeg')
+    {
+        cb(null,true)
+    }
+    else{
+        cb(null,false)
+    }
+  }
+  const upload = multer({ storage: storage ,fileFilter:fileFilter})
+
+router.route("/").get(getAllOriginals).post(upload.single("productImage"),postOriginals)
 
 
-router.route("/").get(getAllOriginals).post(postOriginals)
+router.route("/:Originalsid").get(getEachOriginals).delete(deleteOriginals).patch(upload.single("productImage"),UpdateOriginals)
 
-
-router.route("/:Originalsid").get(getEachOriginals).delete(deleteOriginals)
-router.route("/:Originalsid/:update").patch(UpdateOriginals)
 
 module.exports=router
