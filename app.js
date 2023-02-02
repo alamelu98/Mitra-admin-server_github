fc33b2b380a2f93d4cce1063b116cba41a44f8a6
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2023-02-02 16:06:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,11 +49,12 @@ app.get("/",(req,res)=>
 })
 
 app.use('/uploads',express.static('uploads'))
-app.use("/admin",adminRoute)
+
 app.use("/user",userSignupRoute)
 app.use("/admin/prints",adminAuth,printRoute)
 app.use("/prints",printRoute)
 app.use("/admin/users",adminAuth,userview)
+app.use("/admin",adminRoute)
 app.use("/admin/originals",adminAuth,originalsRoute)
 app.use("/originals",originalsRoute)
 app.use("/admin/cart",adminAuth,cartRoute)
