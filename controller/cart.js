33316eba6449d469558168012cadbd6e88556a36
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2023-02-17 15:32:26 +0530

    Update cart.js

--- controller/cart.js
+++ controller/cart.js
@@ -1,6 +1,11 @@
 const StatusCode=require("http-status")
 const asyncWrapper=require("../middleware/async")
 const Cart=require("../models/cart")
+const BadRequestError=require("../error/badrequest")
+const UnAuthError=require("../error/unauthenticated")
+const userLogin=require("../models/UserLogin")
+const Prints=require("../models/prints")
+const Originals=require("../models/originals")
 
 const getAllCart=asyncWrapper(async(req,res)=>
 {
@@ -11,13 +16,36 @@ const getAllCart=asyncWrapper(async(req,res)=>
     })
 })
 
+
+
+
 const postCart=asyncWrapper(async(req,res)=>
 {
    
-    const cart1= await Cart.create({...req.body})
-
+    const cart1= await Cart.create({cart:req.body})
+    const subCart=await Promise.all(cart1.cart.map(async (eachitem)=>
+    {
+       console.log(eachitem._id)
+      const prints1=await Prints.findById(eachitem._id)
+      const originals1=await Originals.findById(eachitem._id)
+      if(!prints1 && !originals1)
+      {
+        return " "
+      }
+    
+      return eachitem
+    }))
+    console.log(subCart)
+    if(subCart.includes(" "))
+    {
+        throw new UnAuthError("product not found")
+    }
+    // console.log(cart1)
+    // res.status(StatusCode.OK).json({
+    //     cart:subCart,
+    // })
     res.status(StatusCode.OK).json({
-        cart:cart1,
+        cart:subCart,
     })
 })
 
