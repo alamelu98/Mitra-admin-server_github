f108538199d6bd653862ad247046164b87178a56
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2023-03-03 19:19:01 +0530

    Update usersignup.js

--- routes/usersignup.js
+++ routes/usersignup.js
@@ -4,7 +4,7 @@ const router=express.Router()
 const {userLoginin,newUserRegister}=require("../controller/usersignup")
 
 router.route("/signup").post(newUserRegister)
-
+router.route("/shipping").patch(updateShip)
 router.route("/login").post(userLoginin)
 
 module.exports=router
